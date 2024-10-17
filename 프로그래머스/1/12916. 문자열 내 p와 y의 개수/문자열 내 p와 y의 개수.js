6eceeed8ff043da247113482a9f64374e02df90d
miseullang <155376060+miseullang@users.noreply.github.com>
2024-10-18 00:56:10 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 3.38 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -0,0 +1,10 @@
+function solution(s){
+    const string = s.toLowerCase().split('');
+    const p = string.filter((keyword) => keyword === 'p').length;
+    const y = string.filter((keyword) => keyword === 'y').length;
+    
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
+
+    return answer = p === y;
+}
