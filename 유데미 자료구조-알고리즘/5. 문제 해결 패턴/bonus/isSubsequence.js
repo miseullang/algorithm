31dc0449c7bf297185af62d6040b13d2cb198e39
miseullang <miseullang@gmail.com>
2025-06-21 18:45:18 +0900

    250621 유데미 자료구조-알고리즘/05 문제 해결 패턴-다중 포인터 문제(3)

---
+++ 유데미 자료구조-알고리즘/5. 문제 해결 패턴/bonus/isSubsequence.js
@@ -0,0 +1,31 @@
+// 다중 포인터 - isSubsequence
+// 두 문자열을 받아 첫 번째 문자열의 문자가 두 번째 문자열의 문자의 일부에 포함되는지 확인하는 isSubsequence라는 함수를 작성합니다.
+// 즉, 이 함수는 첫 번째 문자열의 문자가 순서가 바뀌지 않고 두 번째 문자열의 어딘가에 나타나는지 확인해야 합니다.
+
+// 예시:
+// isSubsequence('hello', 'hello world'); // true
+// isSubsequence('sing', 'sting'); // true
+// isSubsequence('abc', 'abracadabra'); // true
+// isSubsequence('abc', 'acb'); // false (order matters)
+
+// 솔루션에는 최소한 다음과 같은 복잡성이 있어야 합니다:
+// Time Complexity - O(N + M)
+// Space Complexity - O(1)
+
+function isSubsequence(str1, str2) {
+  // good luck. Add any arguments you deem necessary.
+  let i = 0;
+  let j = 0;
+  if (!str1) return true;
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
+
+console.log(isSubsequence("hello", "hello world")); // true
+console.log(isSubsequence("sing", "sting")); // true
+console.log(isSubsequence("abc", "abracadabra")); // true
+console.log(isSubsequence("abc", "acb")); // false (order matters)
