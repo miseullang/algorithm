c9c44427fbb67543ae19043f13d7195e9fe9cea6
miseullang <miseullang@gmail.com>
2025-06-24 23:45:13 +0900

    250623 유데미 자료구조-알고리즘/09 재귀 보너스 문제 - 문제6(수정)

--- 유데미 자료구조-알고리즘/9. 재귀 보너스 문제/nestedEvenSum.js
+++ 유데미 자료구조-알고리즘/9. 재귀 보너스 문제/nestedEvenSum.js
@@ -2,7 +2,7 @@
 // nestedEvenSum이라는 재귀 함수를 작성하시오.
 // 중첩된(nested) 객체를 포함할 수 있는 객체에서 모든 짝수의 합계를 반환하는 함수입니다.
 
-function nestedEvenSum () {
+function nestedEvenSum (obj) {
   // add whatever parameters you deem necessary - good luck!
    let sum = 0;
 
