76b1c9e68a01a98361f113f664181f91f2d9f445
miseullang <155376060+miseullang@users.noreply.github.com>
2025-06-28 22:29:32 +0900

    [level 1] Title: 폰켓몬, Time: 0.58 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=javascript) 
 
 ### 성능 요약
 
-메모리: 34.1 MB, 시간: 0.56 ms
+메모리: 34.4 MB, 시간: 0.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 23일 03:31:50
+2025년 06월 28일 22:29:28
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,16 +1,6 @@
 function solution(nums) {
-    const unique = new Set(nums).size; // 중복 제거된 폰켓몬 종류 수
-    
-    return Math.min(nums.length / 2, unique);
+  const numSet = new Set(nums);
+  const n = nums.length;
+  const k = n / 2; 
+  return Math.min(k, numSet.size);
 }
-
-// 문제 정의
-// 폰켓몬은 중복이 존재
-// 선택할 수 있는 최대 종류 수를 반환
-// 1. 중복 제거
-// 2. 최댓값 : nums.length / 2
-
-
-// 의사 코드
-// 1. 배열의 중복 제거 (Set 이용)
-// 2. 선택 가능한 폰켓몬 수(N/2)와 중복 제거된 폰켓몬 종류 수 중 작은 값 반환
