cccba2bbf0b3a80106c2c0b539d4accfc0f64b2d
miseullang <155376060+miseullang@users.noreply.github.com>
2025-08-31 09:28:29 +0900

    [level 0] Title: 접미사 배열, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -1,3 +1,12 @@
 function solution(my_string) {
     return [...my_string].map((_, i) => my_string.slice(i)).sort();
 }
+
+// function solution(my_string) {
+//     const answer = [];
+    
+//     for (let i = 0; i < my_string.length; i++) {
+//         answer.push(my_string.slice(i));
+//     }
+    
+//     return answer.sort();
