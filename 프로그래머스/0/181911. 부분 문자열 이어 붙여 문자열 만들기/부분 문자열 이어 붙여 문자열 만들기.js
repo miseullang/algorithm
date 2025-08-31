58c31e7d48f9a5f3b63f089787b5c91154d9afe5
miseullang <155376060+miseullang@users.noreply.github.com>
2025-08-31 09:39:05 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -0,0 +1,8 @@
+function solution(my_strings, parts) {
+    return my_strings
+        .map((str, i) => {
+            const [start, end] = parts[i];
+            return str.slice(start, end + 1);
+        })
+        .join("");
+}
