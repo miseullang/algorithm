d5a9cd4167438078ad09dd8aed77519f8f083cf8
miseullang <155376060+miseullang@users.noreply.github.com>
2025-11-03 22:47:15 +0900

    [level 2] Title: 귤 고르기, Time: 88.32 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,28 +1,18 @@
 function solution(k, tangerine) {
-    const dict = {};
-    tangerine.forEach(value => {
-        dict[value] = (dict[value] || 0) + 1;
-    });
+    let sizeFilter = {};
     
-    const sorted = Object.values(dict).sort((a, b) => b - a);
-    
-    let sum = 0;
-    let answer = 0;
-    
-    for(let count of sorted) {
-        answer++;
-        sum += count;
-        if(sum >= k) break;
+    for (let gyul of tangerine) {
+        sizeFilter[gyul] = (sizeFilter[gyul] || 0) + 1;
     }
     
+    const counts = Object.values(sizeFilter).sort((a, b) => b - a);
+    
+    let answer = 0; // 종류의 수
+    for (let count of counts) {
+        k -= count;
+        answer++;
+        if (k <= 0) return answer;
+    }
+
     return answer;
 }
-
-// 문제 정의
-// 귤을 크기별로 분류
-// 귤 k개를 고를 때 크기별 종류의 최솟값
-
-// 의사 코드
-// tangerine 배열의 값을 키로 갖는 객체 dict 구현
-// dict의 value들만 배열로 만들어서 내림차순 정렬 
-// 정렬된 배열을 순회하면서 k개를 채우는데 필요한 종류 개수 세기
