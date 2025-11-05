d1b1ba113e1cc41da4c4abe2b0b85580ec76ec53
miseullang <155376060+miseullang@users.noreply.github.com>
2025-11-05 22:15:43 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,10 +1,4 @@
 function solution(array, commands) {
     var answer = [];
-    commands.map((val) => {
-        const slicedArr = array.slice(val[0] - 1, val[1]);
-        const sortedArr = slicedArr.sort((a,b) => a - b);
-        answer.push(sortedArr[val[2] - 1]);   
-    })
-    
     return answer;
 }
